f8a230fbc1fd08ccef31e66578cc0c84b8269029
rushikesh <rushikeshsobale@gmail.com>
2025-09-22 20:37:40 +0530

    noq complte build

--- frontend/src/pages/AuthForms.js
+++ frontend/src/pages/AuthForms.js
@@ -142,8 +142,7 @@ const AuthPage = () => {
     }
   };
   const handleSocialLogin = (provider) => {
-    console.log(process.env.REACT_APP_API_URL, provider,   'practice')
-    window.location.href = `${process.env.REACT_APP_API_URL}/auth/${provider}`;
+    window.location.href = `${process.env.REACT_APP_GOOGLE_API_URL || process.env.REACT_APP_API_URL}/auth/${provider}`;
   };
   return (
     <div className="auth-container">
