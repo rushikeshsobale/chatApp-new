33d1e7b7df26ceacde3e672730bacb074ad758b9
Rushikesh Sobale <rushikeshsobale@gmail.com>
2026-04-25 15:31:21 +0530

    fff

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:3000",
-    "https://hibuddy.vercel.app"
+    "https://chatapp-new-1.onrender.com"
   ],
   credentials: true
 }));
