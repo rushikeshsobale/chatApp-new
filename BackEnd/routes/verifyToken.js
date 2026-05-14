f0bea8e652f74b5183bfebe7a8abb12c97daec40
Rushikesh Sobale <rushikeshsobale@gmail.com>
2026-05-14 15:37:19 +0530

    commit

--- BackEnd/routes/verifyToken.js
+++ BackEnd/routes/verifyToken.js
@@ -1,22 +1,31 @@
-const jwt = require('jsonwebtoken');
-const secretKey = process.env.JWT_SECRET || "mySecreateKey";
+const jwt = require("jsonwebtoken");
+
+const secretKey =
+  process.env.JWT_SECRET || "mySecreateKey";
+
 const verifyToken = (req, res, next) => {
-  let token = req.headers['authorization'];
-  
-  if (!token) {
-      return res.status(401).json({ message: "Token not provided" });
-  }
-    // Handle Bearer token format
-    if (token.startsWith("Bearer ")) {
-      token = token.split(" ")[1]; // Extract actual token
-    }
   try {
+    // Get token from cookies
+    const token = req.cookies.token;
+
+    if (!token) {
+      return res.status(401).json({
+        message: "Token not provided",
+      });
+    }
+
     const decoded = jwt.verify(token, secretKey);
-    req.decoded = decoded; 
+
+    req.decoded = decoded;
+
     next();
   } catch (error) {
-    console.error('Error verifying token:', error);
-    res.status(401).json({ message: "Invalid token" });
+    console.error("Error verifying token:", error);
+
+    return res.status(401).json({
+      message: "Invalid token",
+    });
   }
 };
+
 module.exports = verifyToken;
